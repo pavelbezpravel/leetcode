e48563b92b8779bb9ff709294fbdd7b93abcd50f
pavelbezpravel <p.talashchenko@gmail.com>
2024-02-27 00:48:47 +0300

    feat: enable g++ flags

---
+++ cpp/cmake/flags.compiler.gnu.cmake
@@ -0,0 +1,13 @@
+set(PAVELBEZPRAVEL_LEETCODE_CXX_COMPILER_FLAGS
+    -Werror
+    -Wall
+    -Wextra
+    -Wcast-align
+    -Wcast-qual
+    -Wconversion
+    -Wextra-semi
+    -Wsign-compare
+    -Wsign-conversion
+    -pedantic
+    -pedantic-errors
+)
