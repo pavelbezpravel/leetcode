107da6144349df8d7f140cfffb35f71a68cb2b45
pavelbezpravel <p.talashchenko@gmail.com>
2024-02-26 23:10:01 +0300

    feat: submit solution for problem 217
    https://leetcode.com/problems/contains-duplicate/description/

---
+++ cpp/src/contains_duplicate.cpp
@@ -0,0 +1,33 @@
+#include <set>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::set<int> seen{};
+        for (const auto& num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.emplace(num);
+        }
+        return false;
+    }
+};
+
+TEST(containsDuplicate, example1) {
+    auto nums = std::vector<int>{1, 2, 3, 1};
+    EXPECT_EQ(Solution().containsDuplicate(nums), true);
+}
+
+TEST(containsDuplicate, example2) {
+    auto nums = std::vector<int>{1, 2, 3, 4};
+    EXPECT_EQ(Solution().containsDuplicate(nums), false);
+}
+
+TEST(containsDuplicate, example3) {
+    auto nums = std::vector<int>{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+    EXPECT_EQ(Solution().containsDuplicate(nums), true);
+}
